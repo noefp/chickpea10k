3f4f169d4acd9e6f717d7c5d8e6f7407568ea0e0
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2025-01-27 12:49:20 +0100

    about contact cards added

--- egdb_custom_text/about.php
+++ egdb_custom_text/about.php
@@ -1,19 +1,28 @@
 <br>
 <div class="row">
-	<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-    <h3>ChickPea<span style="color:#EFB50B">10k</span> Project Leader</h3>
-		<p><b>Prof. Rajeev Varshney</b><br>
-		Professor at Food Futures Institute, Murdoch University, Perth, Australia</p>
-		<p>Website: <a href="https://researchportal.murdoch.edu.au/esploro/profile/rajeev_varshney/overview" target="_blank">murdoch.edu.au</a></p>
+	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
+		<h3>ChickPea<span style="color:#EFB50B">10k</span> Project Leader</h3>
+		<a class="card flex-row egdb_person_card" href="https://researchportal.murdoch.edu.au/esploro/profile/rajeev_varshney/" target="blank" style="color:#333">
+			<img class="card-img-left egdb_person_img" src="/chickpea10k/egdb_images/people/rajeev.jpg" alt="Lab member">
+			<div class="card-body" style="padding: 5px;">
+				<h4 style="margin-bottom: 5px">Rajeev Varshney</h4>
+				<p class="person-card-text">Professor
+					<br> Food Futures Institute, Murdoch University
+					<br> Perth, Australia</p>
+			</div>
+		</a>
 	</div>
-	<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-    <h3>ChickPea<span style="color:#EFB50B">10k</span> Site Developer</h3>
-    
-		<p><b>Dr. Noe Fernandez-Pozo</b><br>
-		Tenured Scientist at Institute for Mediterranean and Subtropical Horticulture "La Mayora", Malaga, Spain</p>
-    <!-- <p>Campus de Teatinos (UMA-CSIC),<br>
-    Av. Louis Pasteur 49, 29010, Malaga, Spain,</p> -->
-		<p>Website: <a href="https://www.ihsm.uma-csic.es/investigadores/1031/" target="_blank">ihsm.uma-csic.es</a></p>
+	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
+		<h3>ChickPea<span style="color:#EFB50B">10k</span> Site Developer</h3>
+		<a class="card flex-row egdb_person_card" href="https://www.ihsm.uma-csic.es/investigadores/1031/" target="blank" style="color:#333">
+			<img class="card-img-left egdb_person_img" src="/chickpea10k/egdb_images/people/noe.jpeg" alt="Lab member">
+			<div class="card-body" style="padding: 5px;">
+				<h4 style="margin-bottom: 5px">Noe Fernandez-Pozo</h4>
+				<p class="person-card-text">Tenured Scientist
+					<br> Institute for Mediterranean and Subtropical Horticulture "La Mayora"
+					<br> Malaga, Spain</p>
+			</div>
+		</a>
 	</div>
 </div>
 
@@ -29,3 +38,35 @@
 <hr>
 <h3>How to cite Chickpea<span style="color:#EFB50B;">10K</span></h3>
 <br>
+
+<style>
+  .egdb_person_card {
+    min-height:150px;
+    margin-right: 5px;
+    margin-bottom: 5px;
+    border: 1px solid #ddd;
+    padding: 10px 10px 10px;
+		display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+
+  .person-card-text {
+    margin-bottom:2px;
+  }
+  .egdb_person_img {
+    height:150px;
+    object-fit: scale-down;
+		margin-right: 10px;
+  }
+
+  .egdb_person_card a:link {
+    color:#333;
+  }
+  .egdb_person_card:hover {
+    color:#333;
+    border-color: #000;
+    cursor:pointer;
+    text-decoration:none;
+  }
+</style>
