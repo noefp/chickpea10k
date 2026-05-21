44afe26a324d40a8b1231e17238649a9e7fd92d0
Noé Fernández Pozo <noeisneo@gmail.com>
2026-05-21 10:46:27 +0200

    navigation reorganized and passport link fixed

--- egdb_custom_text/custom_toolbar.php
+++ egdb_custom_text/custom_toolbar.php
@@ -1,20 +1,20 @@
 <a class="navbar-brand" href="/easy_gdb/index.php">Chickpea<span style="color:#ffc107"> 10K</span></a>
 
-<li class="nav-item"><a class="nav-link" href="/easy_gdb/tools/passport/02_pass_file_to_datatable.php">Passport</a></li>
+<li class="nav-item"><a class="nav-link" href="/easy_gdb/tools/passport/02_pass_file_to_datatable.php?dir_name=chickpea">Passport</a></li>
 
 <li class="nav-item dropdown">
-  <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">Search</a>
+  <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">Genotype and Phenotype</a>
   <div class="dropdown-menu">
-    <a class="dropdown-item" href="/easy_gdb/tools/search/search_input.php">Search gene annotations</a>
-    <a class="dropdown-item" href="/easy_gdb/tools/passport/passport_search_input.php">Search passport and phenotype</a>
-    <a class="dropdown-item" href="/easy_gdb/tools/vcf/vcf_extract_input.php">Search genetic variation</a>
-    <a class="dropdown-item" href="/easy_gdb/tools/vcf/vcf_extract_acc_list_input.php">Search genetic variation by acc</a>
+    <a class="dropdown-item" href="/easy_gdb/tools/vcf/vcf_extract_input.php">Variant Explorer</a>
+    <a class="dropdown-item" href="/easy_gdb/tools/vcf/vcf_extract_acc_list_input.php">Genetic Variation Search by acc</a>
+    <a class="dropdown-item" href="/easy_gdb/tools/passport/passport_search_input.php">Passport and Phenotype Search</a>
   </div>
 </li>
 
 <li class="nav-item dropdown">
   <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">Tools</a>
   <div class="dropdown-menu">
+    <a class="dropdown-item" href="/easy_gdb/tools/search/search_input.php">Search gene annotations</a>
     <a class="dropdown-item" href="/easy_gdb/tools/blast/blast_input.php">BLAST</a>
     <a class="dropdown-item jbrowse_link" href="/jbrowse/?data=data/cicer_arietinum" target="_blank">Genome browser</a>
     <a class="dropdown-item" href="/easy_gdb/custom_view.php?file_name=synteny_viewer.php">Synteny viewer</a>
